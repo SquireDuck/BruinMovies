cf88d5a3cf691ac246b100785cdd779b5b74500d
SquireDuck <danieljz23@g.ucla.edu>
2024-10-27 23:24:53 -0700

    Minor Bug Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -13,7 +14,9 @@ export default function Home() {
     console.log("Email: ", email);
     console.log("Password ", password);
 
-
+    setUsername("");
+    setEmail("");
+    setPassword("");
   }
 
   const handleSignUp = () => {
@@ -21,18 +24,27 @@ export default function Home() {
   }
 
   return (
+    <>
     <div className="justify-center text-center">
       <h1 className="font-bold text-6xl pt-20">Sign In!</h1> 
       <form className="pt-10 flex flex-col mx-28">
         <input className="my-3 border-2 border-black rounded-lg p-2" type="text" placeholder="Username" value={username}/>
         <input className="my-3 border-2 border-black rounded-lg p-2" type="text" placeholder="Email" value={email}/>
         <input className="my-3 border-2 border-black rounded-lg p-2" type="password" placeholder="Password" value={password}/>
-        <button className=" my-6 border-2 border-black rounded-lg p-2 bg-blue-500 text-white" onClick={handleSignIn}>Sign In</button>
+        <Link href="">
+          <button className=" my-6 px-5 border-2 border-black rounded-lg p-2 bg-blue-500 text-white" onClick={handleSignIn}>Sign In</button>
+        </Link>
       </form>
       <div className="flex flex-row justify-center text-center">
         <p>Don&apos;t have an account?</p>
-        <button className="pl-3 border-0 border-black rounded-lg text-blue text-decoration-line: underline" onClick={handleSignUp}>Sign Up</button>
+        <Link href="" >
+          <button className="text-blue-700 pl-3 border-0 border-black rounded-lg text-decoration-line: underline" onClick={handleSignUp}>Sign Up</button>
+        </Link>
       </div>
     </div>
+    <div className="justify-center text-center pt-20">
+      <button className="font-extrabold py-5 px-14 border-2 border-black rounded-lg p-2 bg-gray-400 text-black" onClick={handleSignIn}>Sign in with Google</button>
+    </div>
+    </>
   );
 }
