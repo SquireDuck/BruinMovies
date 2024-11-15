0d496da03763fab755fcc55f2d26f7d5be077bb7
VictorBC3 <103015909+VictorBC3@users.noreply.github.com>
2024-11-15 14:43:26 -0800

    images are in an array

--- app/page.tsx
+++ app/page.tsx
@@ -3,35 +3,61 @@ import React from "react";
 import Image from "next/image";
 
 export default function Home() {
-const data = [
-  {
 
-    imageLink:
-      "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-  },
-  {
-    imageLink:
-      "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-  },
-  {
-    imageLink:
-      "https://material-taillwind-pro-ct-tailwind-team.vercel.app/img/content2.jpg",
-  },
-  
-  
-];
+
+  const data = [
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    },
+    {
+      imageLink:
+        "https://demos.creative-tim.com/material-kit-pro/assets/img/examples/blog5.jpg",
+    },
+    {
+      imageLink:
+        "https://material-taillwind-pro-ct-tailwind-team.vercel.app/img/content2.jpg",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1620064916958-605375619af8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1493&q=80",
+    },
+  ];
+
   return (
-    <div className="grid grid-cols-3 gap-0 w-24">
-      {data.map(({ imageLink }, index) => (
-        <div key={index}>
-          <img
-            className=""
-            src={imageLink}
+    
+      <section className="pt-10 my-3 grid grid-cols-4 auto-rows[2] flex flex-row justify-center text-center border-2 border-black" >
+        {data.map(({imageLink}, index) => (
+          <Image 
+            loader={()=> imageLink}
+            src={imageLink} 
+            alt="image"
+            width = {350}
+            height = {350}
             
-            alt="gallery-photo"
           />
-        </div>
-      ))}
-    </div>
+        ))}
+      </section>
+
   );
 }
